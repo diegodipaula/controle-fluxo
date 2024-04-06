59673de4f5898746106b6de6805df40ff1b841e4
Diego Di Paula <depaulo.diego@gmail.com>
2024-04-06 18:55:41 -0300

    Inlcuindo condicionais

--- src/ResultadoEscolar.java
+++ src/ResultadoEscolar.java
@@ -16,9 +16,9 @@ public class ResultadoEscolar {
 //        String resultado = nota >=7 ? "Aprovado" : "Reprovado";
 //        System.out.println(resultado);
 
-        int nota = 6;
-        String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
-        System.out.println(resultado);
+//        int nota = 6;
+//        String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+//        System.out.println(resultado);
 
     }
 }
